ecce68df94cf77802781699024de0b48e6518e30
AragAgg <aragaggrawal@gmail.com>
2023-10-10 13:13:00 +0530

    Add solution: Cow Dance Show (.cpp)

---
+++ solutions/Cow Dance Show.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Type definitions for convenience
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef pair<int, int> pii;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef unordered_set<int> usi;
+typedef unordered_map<int, int> umii;
+
+// Shortcuts for common operations
+#define pb push_back
+#define ppb pop_back
+#define f first
+#define s second
+#define all(x) (x).begin(), (x).end()
+#define ll long long
+
+// Debugging macros
+#define debug(x) cerr << #x << " = " << (x) << '\n'
+#define debug_vector(v) _debug_vector(#v, v)
+template<typename T>
+void _debug_vector(const string& name, const vector<T>& a) {
+    cerr << name << " = [ ";
+    for(const auto &x : a) cerr << x << ' ';
+    cerr << "]\n";
+}
+
+// I/O redirection for local testing
+#define iofile(io) \
+        freopen((io + ".in").c_str(), "r", stdin); \
+        freopen((io + ".out").c_str(), "w", stdout);
+
+// Common outputs
+void yes() { cout << "YES" << '\n'; }
+void no() { cout << "NO" << '\n'; }
+
+
+vi tab;
+int t;
+
+bool isVaible(int k){
+    debug(k);
+    priority_queue<int, vi, greater<int>>pq;
+    
+    int iter = 0;
+    int prevTime = 0;
+    int currTime = 0;
+    for(int i = 0; i < min(k, (int)tab.size()); i++){
+        pq.push(tab[iter]);
+        currTime = max(currTime, (tab[iter++])); // buggy
+    }
+    
+    if(iter == tab.size()) return (currTime <= t);
+    
+    while(iter != tab.size()){
+        int prevTime = pq.top();
+        pq.pop();
+        pq.push(prevTime + tab[iter]);
+        currTime = max(currTime, prevTime + tab[iter++]); // <- buggy
+    }
+    
+    return (currTime <= t);
+}
+
+void fx(){
+    int n;
+    cin >> n >> t;
+    
+    tab.assign(n, 0);
+    
+    for(auto &i: tab) cin >> i;
+    
+    int low = 1;
+    int high = n+1;
+    
+    while(low < high){
+        int mid = low + (high - low)/2;
+        
+        if(isVaible(mid)) high = mid;
+        else low = mid+1;
+    }
+    
+    cout << low << endl;
+}
+
+int main() {
+    // Uncomment the following lines for file I/O
+    iofile(string("cowdance"));
+    
+    // Uncomment the following lines for multiple test cases
+    // int t; cin >> t;
+    // while(t--) fx();
+    
+    // Single test case
+    fx();
+    
+    return 0;
+}
