ed89c0740234d0c669e2a666d56f4cb763ed6b53
AragAgg <aragaggrawal@gmail.com>
2023-10-10 13:13:00 +0530

    Add solution: Garland (.cpp)

---
+++ solutions/Garland.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Type definitions for convenience
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef pair<int, int> pii;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef unordered_set<int> usi;
+typedef unordered_map<int, int> umii;
+
+// Shortcuts for common operations
+#define pb push_back
+#define ppb pop_back
+#define f first
+#define s second
+#define all(x) (x).begin(), (x).end()
+
+// Debugging macros
+#define debug(x) cerr << #x << " = " << (x) << '\n'
+#define debug_vector(v) _debug_vector(#v, v)
+template<typename T>
+void _debug_vector(const string& name, const vector<T>& a) {
+    cerr << name << " = [ ";
+    for(const auto &x : a) cerr << x << ' ';
+    cerr << "]\n";
+}
+
+// I/O redirection for local testing
+#define iofile(io) \
+        freopen((io + ".in").c_str(), "r", stdin); \
+        freopen((io + ".out").c_str(), "w", stdout);
+
+// Common outputs
+void yes() { cout << "YES" << '\n'; }
+void no() { cout << "NO" << '\n'; }
+
+vvi memo;
+
+void fx() {
+    int n;
+    cin >> n;
+    
+    string s;
+    cin >> s;
+    
+    n = s.size();
+    
+    memo.assign(n+1, vi(26, -1));
+    
+    int q;
+    cin >> q;
+    
+    while(q--){
+        int m;
+        char c;
+        
+        cin >> m >> c;
+        
+        if(memo[m][c-'a'] != -1){cout << memo[m][c-'a'] << endl; continue;}
+        
+        int left = 0;
+        int right = 0;
+        
+        int usedM = 0;
+        
+        int ans = 0;
+        
+        if(s[0] != c) usedM++;
+        
+        while(right < n-1){
+            while(right < n-1 and ((m-usedM) or s[right+1] == c)){
+                right++;
+                if(s[right] == c) continue;
+                usedM++;
+            }
+            
+            ans = max(ans, right - left+1);
+            
+            while(m == usedM){
+                if(s[left++] == c) continue;
+                usedM--;
+            }
+        }
+        
+        memo[m][c-'a'] = max(ans, 1);
+        
+        cout << max(ans, 1) << endl;
+    }
+    
+}
+
+int main() {
+    // Uncomment the following lines for file I/O
+    // iofile(string("hello"));
+    
+    // Uncomment the following lines for multiple test cases
+    // int t; cin >> t;
+    // while(t--) fx();
+    
+    // Single test case
+    fx();
+    
+    return 0;
+}
