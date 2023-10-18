b8581b92669dc8a90aa8d0425d10f6600acbeed7
AragAgg <aragaggrawal@gmail.com>
2023-10-18 11:41:00 +0530

    Add solution: PolandBall and Forest (.cpp)

---
+++ solutions/PolandBall and Forest.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Shortcuts for common operations
+#define pb push_back
+#define ppb pop_back
+#define f first
+#define s second
+#define all(x) (x).begin(), (x).end()
+#define ll long long
+
+// Type definitions for convenience
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef pair<int, int> pii;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef unordered_set<int> usi;
+typedef unordered_map<int, int> umii;
+
+// Debugging macros
+#define debug(x) cerr << #x << " = " << (x) << '\n'
+#define debug_vector(v) _debug_vector(#v, v)
+template<typename T>
+void _debug_vector(const string& name, const vector<T>& a) {
+    cerr << name << " = [ ";
+    for(const auto &x : a) cerr << x << ' ';
+    cerr << "]\n";
+}
+
+// I/O redirection for local testing
+#define iofile(io) \
+        freopen((io + ".in").c_str(), "r", stdin); \
+        freopen((io + ".out").c_str(), "w", stdout);
+
+// delta for floodfill
+vi dx = {0, 1, 0, -1};
+vi dy = {1, 0, -1, 0};
+
+// extended deltas for floodfill
+vi edx = {0, 1, 0, -1, 1, 1, -1, -1};
+vi edy = {1, 0, -1, 0, 1, -1, 1, -1};
+
+// Common outputs
+void yes() { cout << "YES" << '\n'; }
+void no() { cout << "NO" << '\n'; }
+
+void fx() {
+    int n;
+    cin >> n;
+    
+    vvi tab(n);
+    for(int i = 0; i < n; i++){
+        int t;
+        cin >> t;
+        t--;
+        tab[i].pb(t);
+        tab[t].pb(i);
+    }
+    
+    vi visited(n, 0);
+    int ans = 0;
+    
+    for(int i = 0; i < n; i++){
+        if(visited[i]) continue;
+        
+        ans++;
+        
+        queue<int>q;
+        q.push(i);
+        
+        while(!q.empty()){
+            int curr = q.front();
+            q.pop();
+            
+            if(visited[curr]) continue;
+            visited[curr] = true;
+            for(auto neigh: tab[curr]) q.push(neigh);
+        }
+    }
+    
+    cout << ans << endl;
+    
+}
+
+int main() {
+    // Uncomment the following lines for file I/O
+    // iofile(string("hello"));
+    
+    // Uncomment the following lines for multiple test cases
+    // int t; cin >> t;
+    // while(t--) fx();
+    
+    // Single test case
+    fx();
+    
+    return 0;
+}
