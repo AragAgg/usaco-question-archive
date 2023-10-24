33bf3705b64ce87f91ff9fbd6f3f927488cb0bc1
AragAgg <aragaggrawal@gmail.com>
2023-10-25 02:27:00 +0530

    Add solution: Lisa and the Martians (.cpp)

---
+++ solutions/Lisa and the Martians.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Shortcuts for common operations
+#define pb push_back
+#define ppb pop_back
+#define f first
+#define s second
+#define all(x) (x).begin(), (x).end()
+#define ll long long
+#define endl "\n"
+
+// Type definitions for convenience
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef pair<int, int> pii;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef unordered_set<int> usi;
+typedef unordered_map<int, int> umii;
+
+// Debugging macros
+#define debug(x) cerr << #x << " = " << (x) << '\n'
+#define debug_vector(v) _debug_vector(#v, v)
+template<typename T>
+void _debug_vector(const string& name, const vector<T>& a) {
+    cerr << name << " = [ ";
+    for(const auto &x : a) cerr << x << ' ';
+    cerr << "]\n";
+}
+
+// I/O redirection for local testing
+#define iofile(io) \
+        freopen((io + ".in").c_str(), "r", stdin); \
+        freopen((io + ".out").c_str(), "w", stdout);
+
+// delta for floodfill
+vi dx = {0, 1, 0, -1};
+vi dy = {1, 0, -1, 0};
+
+// extended deltas for floodfill
+vi edx = {0, 1, 0, -1, 1, 1, -1, -1};
+vi edy = {1, 0, -1, 0, 1, -1, 1, -1};
+
+// Common outputs
+void yes() { cout << "YES" << '\n'; }
+void no() { cout << "NO" << '\n'; }
+
+// cin.tie(0)->sync_with_stdio(0);
+
+void fx() {
+    int n, k;
+    cin >> n >> k;
+    
+    vector<pair<unsigned int, int>> tab(n);
+    for(int i = 0; i < n; i++) cin >> tab[i].f, tab[i].s = i;
+    
+    sort(all(tab));
+    
+    int a = tab[0].s;
+    int b = tab[1].s; //index in input list
+    int besta = 0; // index in tab
+    int bestb = 1;
+    
+    for(int i = 0; i < n-1; i++){
+        if(((tab[i].f ^ ~tab[i+1].f) > (tab[besta].f ^ ~tab[bestb].f))){
+            besta = i;
+            bestb = i+1;
+            a = tab[besta].s;
+            b = tab[bestb].s; 
+        }
+    }
+    
+    // int x = (1 << k)-1;
+    // for(int i = 0; i < k; i++) if(tab[besta].f & (1 << i)) x ^= (1 << i);
+    int x = (~tab[besta].f);
+    x &= (1 << k)-1;
+
+    cout << (a+1) << " " << (b+1) << " " << (x) << endl;
+    
+}
+
+int main() {
+    // Uncomment the following lines for file I/O
+    // iofile(string("hello"));
+    
+    // Uncomment the following lines for multiple test cases
+    int t; cin >> t;
+    while(t--) fx();
+    
+    // Single test case
+    // fx();
+    
+    return 0;
+}
