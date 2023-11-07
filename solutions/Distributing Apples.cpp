a205c5541b41a4c9fed591a04ddf72acef677ef7
AragAgg <aragaggrawal@gmail.com>
2023-11-08 00:18:00 +0530

    Add solution: Distributing Apples (.cpp)

---
+++ solutions/Distributing Apples.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Shortcuts for common operations
+#define pb push_back
+#define ppb pop_back
+#define f first
+#define s second
+#define all(x) (x).begin(), (x).end()
+#define ll long long
+#define endl "\n"
+
+// Type definitions for convenience
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef pair<int, int> pii;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef unordered_set<int> usi;
+typedef unordered_map<int, int> umii;
+
+// Debugging macros
+#define debug(x) cerr << #x << " = " << (x) << '\n'
+#define debug_vector(v) _debug_vector(#v, v)
+template<typename T>
+void _debug_vector(const string& name, const vector<T>& a) {
+    cerr << name << " = [ ";
+    for(const auto &x : a) cerr << x << ' ';
+    cerr << "]\n";
+}
+
+// I/O redirection for local testing
+#define iofile(io) \
+        freopen((io + ".in").c_str(), "r", stdin); \
+        freopen((io + ".out").c_str(), "w", stdout);
+
+// delta for floodfill
+vi dx = {0, 1, 0, -1};
+vi dy = {1, 0, -1, 0};
+
+// extended deltas for floodfill
+vi edx = {0, 1, 0, -1, 1, 1, -1, -1};
+vi edy = {1, 0, -1, 0, 1, -1, 1, -1};
+
+// Common outputs
+void yes() { cout << "YES" << '\n'; }
+void no() { cout << "NO" << '\n'; }
+
+// cin.tie(0)->sync_with_stdio(0);
+ll mod = 1e9+7;
+
+ll fastExpo(ll n, ll m){
+    ll res = 1;
+    while(m){
+        if(m&1) res = res * n % mod;
+        n = n * n % mod;
+        m /= 2;
+    }
+    return res;
+}
+
+ll fact(ll n){
+    ll res = 1;
+    while(n) res = res * n-- % mod;
+    return res;
+}
+
+void fx() {
+    // Functionality for fx
+
+    ll n, m;
+    cin >> n >> m;
+    ll temp = fact(n + m -1);
+    temp = (temp * fastExpo(fact(n-1), mod-2) % mod) * fastExpo(fact(m), mod-2) % mod;
+
+    cout << temp << endl;
+}
+
+
+int main() {
+    // Uncomment the following lines for file I/O
+    // iofile(string("hello"));
+    
+    // Uncomment the following lines for multiple test cases
+    // int t; cin >> t;
+    // while(t--) fx();
+    
+    // Single test case
+    fx();
+    
+    return 0;
+}
