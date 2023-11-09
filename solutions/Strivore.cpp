0a0add74c5eae0e3b4f6b9f4d958b547b2243f2e
AragAgg <aragaggrawal@gmail.com>
2023-11-09 15:02:00 +0530

    Add solution: Strivore (.cpp)

---
+++ solutions/Strivore.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Shortcuts for common operations
+#define pb push_back
+#define ppb pop_back
+#define f first
+#define s second
+#define all(x) (x).begin(), (x).end()
+#define ll long long
+#define endl "\n"
+
+// Type definitions for convenience
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef pair<int, int> pii;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef unordered_set<int> usi;
+typedef unordered_map<int, int> umii;
+
+// Debugging macros
+#define debug(x) cerr << #x << " = " << (x) << '\n'
+#define debug_vector(v) _debug_vector(#v, v)
+template<typename T>
+void _debug_vector(const string& name, const vector<T>& a) {
+    cerr << name << " = [ ";
+    for(const auto &x : a) cerr << x << ' ';
+    cerr << "]\n";
+}
+
+// I/O redirection for local testing
+#define iofile(io) \
+        freopen((io + ".in").c_str(), "r", stdin); \
+        freopen((io + ".out").c_str(), "w", stdout);
+
+// delta for floodfill
+vi dx = {0, 1, 0, -1};
+vi dy = {1, 0, -1, 0};
+
+// extended deltas for floodfill
+vi edx = {0, 1, 0, -1, 1, 1, -1, -1};
+vi edy = {1, 0, -1, 0, 1, -1, 1, -1};
+
+// Common outputs
+void yes() { cout << "YES" << '\n'; }
+void no() { cout << "NO" << '\n'; }
+
+// cin.tie(0)->sync_with_stdio(0);
+ll mod = 1e9+7;
+
+ll fastExpo(ll n, ll m){
+    ll res = 1;
+    while(m){
+        if(m&1) res = res * n %mod;
+        n = n * n %mod;
+        m /= 2;
+    }
+    return res;
+}
+
+ll x = 2e6+100;
+vector<ll> fact(x, 1);
+vector<ll> modinv(x, 1);
+
+ll nchooser(ll n, ll r){
+    ll res = fact[n] * modinv[r] %mod;
+    res = res * modinv[n-r] % mod;
+    return res;
+}
+
+void fx() {
+    // Functionality for fx
+    ll k;
+    string s;
+    cin >> k >> s;
+
+    ll n = k + ((ll)s.size());
+
+    for(int i = 1; i < x; i++) fact[i] = fact[i-1] * i %mod;
+    modinv[x-1] = fastExpo(fact[x-1], mod-2);
+    for(int i = x-2; i >= 0; i--) modinv[i] = (i+1) * modinv[i+1] %mod;
+
+    ll res = fastExpo(26, n);
+
+    for(int i = 0; i < (ll)s.size(); i++) res = (res - (nchooser(n, i) * fastExpo(25, n - i)%mod) + mod) % mod;
+
+    cout << res << endl;
+
+}
+
+int main() {
+    // Uncomment the following lines for file I/O
+    // iofile(string("hello"));
+    
+    // Uncomment the following lines for multiple test cases
+    // int t; cin >> t;
+    // while(t--) fx();
+    
+    // Single test case
+    fx();
+    
+    return 0;
+}
