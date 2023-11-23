1acfc259e0ffc263f9b75cf40b1913dcf3e7a160
AragAgg <aragaggrawal@gmail.com>
2023-11-23 20:53:00 +0530

    Add solution: Removal Game (.cpp)

---
+++ solutions/Removal Game.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Shortcuts for common operations
+#define pb push_back
+#define ppb pop_back
+#define f first
+#define s second
+#define all(x) (x).begin(), (x).end()
+#define ll long long
+#define endl "\n"
+
+// Type definitions for convenience
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef pair<int, int> pii;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef unordered_set<int> usi;
+typedef unordered_map<int, int> umii;
+
+// Debugging macros
+#define debug(x) cerr << #x << " = " << (x) << '\n'
+#define debug_vector(v) _debug_vector(#v, v)
+template<typename T>
+void _debug_vector(const string& name, const vector<T>& a) {
+    cerr << name << " = [ ";
+    for(const auto &x : a) cerr << x << ' ';
+    cerr << "]\n";
+}
+
+// I/O redirection for local testing
+#define iofile(io) \
+        freopen((io + ".in").c_str(), "r", stdin); \
+        freopen((io + ".out").c_str(), "w", stdout);
+
+// delta for floodfill
+vi dx = {0, 1, 0, -1};
+vi dy = {1, 0, -1, 0};
+
+// extended deltas for floodfill
+vi edx = {0, 1, 0, -1, 1, 1, -1, -1};
+vi edy = {1, 0, -1, 0, 1, -1, 1, -1};
+
+// Common outputs
+void yes() { cout << "YES" << '\n'; }
+void no() { cout << "NO" << '\n'; }
+
+// cin.tie(0)->sync_with_stdio(0);
+ll mmax = 1e15;
+ll mmin = -1e15;
+vi tab;
+vector<vector<ll>> dpMAX(5001, vector<ll>(5001, mmin)), dpMIN(5001, vector<ll>(5001, mmax));
+        
+ll fx2(int i, int j, bool flag){
+   if(i == j) return (flag?tab[i]:0); 
+
+   if(flag){
+       if(dpMAX[i][j] != mmin ) return dpMAX[i][j];
+       return dpMAX[i][j] = max(tab[i] + fx2(i+1, j, !flag), tab[j] + fx2(i, j-1, !flag));
+    } else{
+        if(dpMIN[i][j] != mmax) return dpMIN[i][j];
+        return dpMIN[i][j] = min(fx2(i+1, j, !flag), fx2(i, j-1, !flag));
+    }
+}
+
+void fx() {
+    // Functionality for fx
+    int n;
+    cin >> n;
+    tab.assign(n, 0);
+
+    for(auto &i: tab) cin >> i;
+
+    cout << fx2(0, n-1, true) << endl;
+}
+
+int main() {
+    // Uncomment the following lines for file I/O
+    // iofile(string("hello"));
+    
+    // Uncomment the following lines for multiple test cases
+    // int t; cin >> t;
+    // while(t--) fx();
+    
+    // Single test case
+    fx();
+    
+    return 0;
+}
