5b85330fb14034cbc2ef3b62c6f3997d4d46568d
AragAgg <aragaggrawal@gmail.com>
2023-11-23 19:23:00 +0530

    Add solution: Rectangle Cutting (.cpp)

---
+++ solutions/Rectangle Cutting.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Shortcuts for common operations
+#define pb push_back
+#define ppb pop_back
+#define f first
+#define s second
+#define all(x) (x).begin(), (x).end()
+#define ll long long
+#define endl "\n"
+
+// Type definitions for convenience
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef pair<int, int> pii;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef unordered_set<int> usi;
+typedef unordered_map<int, int> umii;
+
+// Debugging macros
+#define debug(x) cerr << #x << " = " << (x) << '\n'
+#define debug_vector(v) _debug_vector(#v, v)
+template<typename T>
+void _debug_vector(const string& name, const vector<T>& a) {
+    cerr << name << " = [ ";
+    for(const auto &x : a) cerr << x << ' ';
+    cerr << "]\n";
+}
+
+// I/O redirection for local testing
+#define iofile(io) \
+        freopen((io + ".in").c_str(), "r", stdin); \
+        freopen((io + ".out").c_str(), "w", stdout);
+
+// delta for floodfill
+vi dx = {0, 1, 0, -1};
+vi dy = {1, 0, -1, 0};
+
+// extended deltas for floodfill
+vi edx = {0, 1, 0, -1, 1, 1, -1, -1};
+vi edy = {1, 0, -1, 0, 1, -1, 1, -1};
+
+// Common outputs
+void yes() { cout << "YES" << '\n'; }
+void no() { cout << "NO" << '\n'; }
+
+// cin.tie(0)->sync_with_stdio(0);
+
+
+ll n, m;
+vector<vector<ll>>dp(501, vector<ll>(501, -1));
+
+ll fx2(int i, int j){
+    if(i > j) swap(i, j);
+    if( i == j  and i >= 0) return 0;
+    if(i <= 0 or j <= 0) return INT_MAX;
+    if(dp[i][j] != -1) return dp[i][j];    
+
+    dp[i][j] = INT_MAX;
+    for(int k = 1; k < j; k++) dp[i][j] = min(dp[i][j], 1 + fx2(i, k) + fx2(i, j - k));
+    for(int k = 1; k < i; k++) dp[i][j] = min(dp[i][j], 1 + fx2(k, j) + fx2(i - k, j));
+    return dp[i][j];
+}
+
+void fx() {
+    // Functionality for fx
+    cin >> n >> m;
+    cout << fx2(n, m) << endl;
+}
+
+int main() {
+    // Uncomment the following lines for file I/O
+    // iofile(string("hello"));
+    
+    // Uncomment the following lines for multiple test cases
+    // int t; cin >> t;
+    // while(t--) fx();
+    
+    // Single test case
+    fx();
+    
+    return 0;
+}
