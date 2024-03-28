141cc3b36af560ea528bc13b1084d8e228d93eb5
AragAgg <aragaggrawal@gmail.com>
2024-03-29 01:02:00 +0530

    Add solution: Mega Inversions (.cpp)

---
+++ solutions/Mega Inversions.cpp
@@ -0,0 +1,106 @@
+//#pragma GCC optimize("03,unroll-loops")
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+
+
+using namespace __gnu_pbds;
+using namespace std;
+
+template<class T>
+using Tree = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+// Shortcuts for common operations
+#define pb push_back
+#define p push
+#define ppb pop_back
+#define f first
+#define s second
+#define all(x) (x).begin(), (x).end()
+#define ll long long
+//#define int ll
+#define endl "\n"
+#define sz(x) (int)x.size()
+
+// Type definitions for convenience
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef vector<bool> vb;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+
+// Debugging macros
+#define debug(x) cerr << #x << " = " << (x) << '\n'
+#define debug_vector(v) _debug_vector(#v, v)
+template<typename T>
+void _debug_vector(const string& name, const vector<T>& a) {
+    cerr << name << " = [ ";
+    for(const auto &x : a) cerr << x << ' ';
+    cerr << "]\n";
+}
+
+// I/O redirection for local testing
+#define iofile(io) \
+        freopen((io + ".in").c_str(), "r", stdin); \
+        freopen((io + ".out").c_str(), "w", stdout);
+
+// delta for floodfill
+vi dx = {0, 1, 0, -1};
+vi dy = {1, 0, -1, 0};
+
+// extended deltas for floodfill
+vi edx = {0, 1, 0, -1, 1, 1, -1, -1};
+vi edy = {1, 0, -1, 0, 1, -1, 1, -1};
+
+// Common outputs
+void yes() { cout << "YES" << '\n'; }
+void no() { cout << "NO" << '\n'; }
+
+
+void fx() {
+    int n;
+    cin >> n;
+
+    Tree<pii> a, b;
+
+    vi tab(n);
+    vll pref(n, 0), suf(n, 0);
+
+    for(auto &i: tab) cin >> i;
+
+    for(int i = 0 ; i < n; i++){
+        pref[i] = i - a.order_of_key({tab[i], INT_MAX});
+        a.insert({tab[i], i});
+    }
+
+    for(int i = n-1; i >= 0; i--){
+        suf[i] = b.order_of_key({tab[i], INT_MIN});
+        b.insert({tab[i], i});
+    }
+    
+    // debug_vector(pref);
+    // debug_vector(suf);
+
+    ll res = 0;
+    for(int i = 0; i < n; i++) res += pref[i] * suf[i];
+    
+    cout << res << endl;
+}
+
+signed main() {
+
+    cin.tie(0)->sync_with_stdio(0);
+    // Uncomment the following lines for file I/O
+    // iofile(string("hello"));
+    
+    // Uncomment the following lines for multiple test cases
+    // int t; cin >> t; while(t--) fx();
+    
+    // Single test case
+    fx();
+    
+    return 0;
+}
